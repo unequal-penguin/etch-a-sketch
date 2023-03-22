fded90eabfe64d44f74f80c1b5b4a62af6d0e406
unequal-penguin <heitor360@live.com>
2023-03-21 23:29:36 -0300

    Add change color to the grid functionality
    - Click to start painting it black
    - Change grid size button
    - Simple styling to the page

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 const canvas = document.querySelector('#canvas');
+const button = document.querySelector('#grid-btn');
 
+let mousePressed = false;
 let squareNumber = 16;
 let canvasSize = canvas.clientHeight;
 
-let squareSize = canvasSize / squareNumber;
+button.addEventListener("click", changeGridSize);
 
-console.log(canvasSize);
-console.log(squareSize);
+function drawGrid(squareNumber) {
+  let squareSize = canvasSize / squareNumber;
 
-for (let i = 0; i < squareNumber; i++) {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  canvas.appendChild(row);
+  for (let i = 0; i < squareNumber; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    canvas.appendChild(row);
 
-  for (let j = 0; j < squareNumber; j++) {
-    const cell = document.createElement('div')
-    cell.classList.add('cell');
-    cell.style.height = `${squareSize}px`;
-    cell.style.width = `${squareSize}px`;
-    row.appendChild(cell);
+    for (let j = 0; j < squareNumber; j++) {
+      const cell = document.createElement('div')
+      cell.classList.add('cell');
+      cell.style.height = `${squareSize}px`;
+      cell.style.width = `${squareSize}px`;
+      cell.addEventListener('mousedown', mouseDown);
+      cell.addEventListener('mouseup', mouseUp);
+      cell.addEventListener('mousemove', changeSquareColor);
+      row.appendChild(cell);
+    }
   }
+}
+
+function changeGridSize() {
+  var input = prompt("Please enter a number:");
+  removeAllChilds(canvas);
+  drawGrid(parseInt(input));
+}
+
+function removeAllChilds(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+function mouseDown() {
+  mousePressed = true;
+  this.style.backgroundColor = 'black';
+}
+
+function mouseUp() {
+  mousePressed = false;
+}
+
+function changeSquareColor() {
+  if (mousePressed)
+    this.style.backgroundColor = 'black';
 }
